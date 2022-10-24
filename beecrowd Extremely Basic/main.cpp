256668f81a0de5b93fbebc521b1d122d1641ca70
Francielly Nascimento <116462191+Francielly-Nascimento@users.noreply.github.com>
2022-10-24 13:21:31 -0300

    Add files via upload

---
+++ beecrowd Extremely Basic/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    int x = a+b;
+
+    printf("x = %d\n", x);
+    return 0;
+}
